48faa3887e02d6ed54b4ab1408e76e47330cf71e
Medhat-Alathmna <medhatalathmna@gmail.com>
2025-11-02 16:35:23 +0200

    0

--- src/api/product/controllers/qty.ts
+++ src/api/product/controllers/qty.ts
@@ -15,14 +15,13 @@ module.exports = ({
         // Update stock quantity
         product.stocks -= qty;
         
-        // Save updated product
-        // const updatedProduct = await strapi.entityService.update('api::product.product', productId, {
-        //     data:{
-        //         stocks:product.stocks
-        //     }
-        // });
+        const updatedProduct = await strapi.entityService.update('api::product.product', productId, {
+            data:{
+                stocks:product.stocks
+            }
+        });
   
-        // ctx.send({ message: 'Stock updated successfully', updatedProduct });
+        ctx.send({ message: 'Stock updated successfully', updatedProduct });
       } catch (err) {
         ctx.send(product);
       }
